78c4dc53fa27c03f5488d973844cdc2927b5897e
faisal <fsl2023devops@gmail.com>
2025-09-15 00:39:03 +0530

    fixed the syntax of docker local deploy

--- vars/DockerDeployLocal.groovy
+++ vars/DockerDeployLocal.groovy
@@ -1,11 +1,11 @@
 def call(Map args = [:]) {
-    def image = args.image ?: error('image name is required')
-    def name = args.name ?: error('container name is required')
+    def IMAGE_NAME = args.IMAGE_NAME ?: error('image name is required')
+    def CONTAINER_NAME = args.CONTAINER_NAME ?: error('container name is required')
     def hostPort = args.hostPort ?: error('port number required')
     def containerPort = args.containerPort ?: error('port number required')
 
-    sh """ docker stop ${name} || true
-           docker rm ${name} || true
-           docker run -d -p ${HostPort}:${containerPort} --name ${name} ${image}
+    sh """ docker stop ${CONTAINER_NAME} || true
+           docker rm ${CONTAINER_NAME} || true
+           docker run -d -p ${HostPort}:${containerPort} --name ${CONTAINER_NAME} ${IMAGE_NAME}
        """   
 }
