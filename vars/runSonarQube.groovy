70494b50f6d625a2b790e9d7c49431baa0c04c02
faisal <fsl2023devops@gmail.com>
2025-09-15 15:53:35 +0530

    upgraded SonarQube function to pull coverage reports as well

--- vars/runSonarQube.groovy
+++ vars/runSonarQube.groovy
@@ -1,8 +1,9 @@
 def call(Map args = [:]) {
     def server = args.server ?: error('sonarqube server name is required')
     def projectKey = args.projectKey ?: error('project key is required')
+    def coverageReport = args.coverageReport ?: 'target/site/jacoco/jacoco.xml'
 
     withSonarQubeEnv(server) {
-        sh " mvn sonar:sonar -Dsonar.projectKey=${projectKey}"
+        sh " mvn sonar:sonar -Dsonar.projectKey=${projectKey} -Dsonar.coverage.jacoco.xmlReportPaths=${coverageReport}"
     }
 }
