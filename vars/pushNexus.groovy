1978c85c28b839db0d5f99b0f0878623cf1c418a
faisal <fsl2023devops@gmail.com>
2025-09-16 18:57:03 +0530

    fixed issue of pushNexus

--- vars/pushNexus.groovy
+++ vars/pushNexus.groovy
@@ -1,13 +1,12 @@
-def call(Map args =[:]) {
-    def serverId = args.serverId ?: error('Nexus server id required')
-    def credentialsId = args.credentialsId ?: error('credentialsId is required')
-    def repository = args.repository ?: error('Nexus repo name is required')
+def call(Map args = [:]) {
+    def serverId     = args.serverId ?: error('settings.xml fileId is required')
+    def credentialsId = args.credentialsId ?: error('Nexus credentialsId is required')
 
     configFileProvider([configFile(fileId: serverId, variable: 'MAVEN_SETTINGS')]) {
-            withCredentials([usernamePassword(credentialsId: credentialsId, 
-                                              usernameVariable: 'NEXUS_USER', 
-                                              passwordVariable: 'NEXUS_PASS')]) {
-                sh "mvn deploy -s $MAVEN_SETTINGS -Dnexus.repo=${repository}"
-    }
+        withCredentials([usernamePassword(credentialsId: credentialsId,
+                                          usernameVariable: 'NEXUS_USER',
+                                          passwordVariable: 'NEXUS_PASS')]) {
+            sh "mvn clean deploy -s $MAVEN_SETTINGS"
+        }
     }
 }
